d96ba44e9b3115f06e80297ad2be9e7dc3975ef2
chetat <yekuwilfred@gmail.com>
2020-09-04 14:11:15 +0100

    Setup initial project structure

---
+++ models.py
@@ -0,0 +1,91 @@
+from app import sqlalchemy as db, create_app
+from json import JSONEncoder
+from datetime import datetime
+from flask_bcrypt import generate_password_hash, check_password_hash
+
+
+user_role = db.Table(
+    "user_role",
+    db.Column("user_id", db.Integer,
+              db.ForeignKey("users.id"),
+              primary_key=True),
+    db.Column("role_id", db.Integer,
+              db.ForeignKey("roles.id"),
+              
+              primary_key=True)
+)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    users = db.relationship("User", 
+                            secondary=user_role,
+                            back_populates="roles",
+                            lazy=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    @property
+    def serialize(self):
+
+        return {
+            "id": self.id,
+            "name": self.name,
+            "users": self.users
+    }
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    roles = db.relationship("Role",
+                            secondary=user_role,
+                            back_populates="users",
+                            cascade="all, delete",
+                            lazy=True)
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    @property
+    def serialize(self):
+        role = Role.query.filter(Role.users.any(id=self.id)).first()
+        return {
+            "id": self.id,
+            "name": self.username,
+            "email": self.email,
+            "role": role.name if role else 'No role assigned'
+        }
